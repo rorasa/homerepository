11cb6eb54295d7e8966369255d343f418f655923
Wattanit Hotrakool <wattanit.h@gmail.com>
2018-01-11 01:09:46 +0700

    Install React-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Grid, Row, Col} from 'react-bootstrap';
+import { BrowserRouter as Router,
+  Route, Link} from 'react-router-dom';
+
 import Navigation from './Navigation';
 import Directory from './Directory';
 import Sidebar from './Sidebar';
@@ -25,7 +28,12 @@ class App extends Component {
               <Sidebar/>
             </Col>
             <Col xs={12} sm={9}>
-              <Directory/>
+              <Router>
+                <div>
+                  <Route exact path="/" component={Sidebar}/>
+                  <Route path="/directory" component={Directory}/>
+                </div>
+              </Router>
             </Col>
           </Row>
         </Grid>
