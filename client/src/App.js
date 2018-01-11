9d89b3526a61c11183159b9d0a5a7d7df0cc5565
Wattanit Hotrakool <wattanit.h@gmail.com>
2018-01-12 00:07:25 +0700

    Remove the sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Grid, Row, Col} from 'react-bootstrap';
 import { BrowserRouter as Router,
   Route, Link} from 'react-router-dom';
 
@@ -18,26 +17,18 @@ class App extends Component {
     return (
       <div className="App">
         <Navigation/>
-        <Grid fluid>
-          <Row>
-            <Col className='App-sidebar' sm={3} xsHidden>
-              <Sidebar/>
-            </Col>
-            <Col xs={12} sm={9}>
-              <Router>
-                <div>
-                  <Route exact path="/" component={NA}/>
-                  <Route exact path="/client" component={Login}/>
-                  <Route exact path="/client/directory" component={Directory}/>
-                  <Route exact path="/client/new-entry" component={Sidebar}/>
-                  <Route path="/client/directory/:collectionName" component={Collection}/>
-                  <Route path="/client/entry/:entryId" component={Entry}/>
-                  <Route exact path="/client/admin" component={AdminMain}/>
-                </div>
-              </Router>
-            </Col>
-          </Row>
-        </Grid>
+
+        <Router>
+          <div className="container">
+            <Route exact path="/" component={NA}/>
+            <Route exact path="/client" component={Login}/>
+            <Route exact path="/client/directory" component={Directory}/>
+            <Route exact path="/client/new-entry" component={Sidebar}/>
+            <Route path="/client/directory/:collectionName" component={Collection}/>
+            <Route path="/client/entry/:entryId" component={Entry}/>
+            <Route exact path="/client/admin" component={AdminMain}/>
+          </div>
+        </Router>
 
         <div className="App-footer">
           <Footer/>
